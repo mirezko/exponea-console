dcf3e63076b2154d3ea393688e7578f0a2d5f0ae
Tomas Mizerak <tomas.mizerak@exponea.com>
2017-04-11 15:45:41 +0200

    readme and hopefully release fix

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# exponea-console
+# EXPONEA Console (by EXPONEA)
 Exponea tracking debug console
+
+# Development
+
+1. `npm install`
+2. `gulp watch`
+
+# Build & Publishing
+
+1. `gulp build`
+2. `gulp package`
+3. Upload to [Chrome Web Store Developer Dashboard](https://chrome.google.com/webstore/developer/dashboard)
+
+# Production release
+
+Latest version is published in Chrome Web Store as [EXPONEA Console](https://chrome.google.com/webstore/detail/exponea-console/hllmpfcdcikgcdlklhdflfcoconilagj)
--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,108 +1,105 @@
-(() => {
-  'use strict';
+'use strict';
 
-  const MESSAGEHISTORY_LIMIT = 200;
+const MESSAGEHISTORY_LIMIT = 200;
 
-  const backgroundPorts = [];
-  const messagesHistory = [];
+const backgroundPorts = [];
+const messagesHistory = [];
 
-  chrome.runtime.onInstalled.addListener(details => {
-    console.log('Extension ' + details.reason, details.previousVersion);
-    chrome.browserAction.setBadgeBackgroundColor({
-      color: '#009900'
-    });
+chrome.runtime.onInstalled.addListener(details => {
+  console.log('Extension ' + details.reason, details.previousVersion);
+  chrome.browserAction.setBadgeBackgroundColor({
+    color: '#009900'
   });
+});
 
-  chrome.runtime.onSuspend.addListener(() => {
-    chrome.browserAction.setBadgeBackgroundColor({
-      color: '##990000'
-    });
+chrome.runtime.onSuspend.addListener(() => {
+  chrome.browserAction.setBadgeBackgroundColor({
+    color: '#990000'
   });
+});
 
-  chrome.runtime.onConnect.addListener((port) => {
-    var match = port.name.match(/background_(\d+)/);
-    if (match.length !== 2) {
-      return;
+chrome.runtime.onConnect.addListener((port) => {
+  var match = port.name.match(/background_(\d+)/);
+  if (match.length !== 2) {
+    return;
+  }
+
+  var tabId = parseInt(match[1]);
+  backgroundPorts[tabId] = port;
+  port.onMessage.addListener((msg) => {
+    switch (msg.type) {
+      case 'history':
+        sendHistory(tabId);
+        break;
+      default:
+        console.warn('unknown message on background port', msg);
+        break;
     }
-
-    var tabId = parseInt(match[1]);
-    backgroundPorts[tabId] = port;
-    port.onMessage.addListener((msg) => {
-      switch (msg.type) {
-        case 'history':
-          sendHistory(tabId);
-          break;
-        default:
-          console.warn('unknown message on background port', msg);
-          break;
-      }
-    });
-    port.onDisconnect.addListener(() => {
-      delete backgroundPorts[tabId];
-    });
   });
+  port.onDisconnect.addListener(() => {
+    delete backgroundPorts[tabId];
+  });
+});
 
-  const filter = { // TODO: get from settings page and store in google.storage.sync
-    urls: [
-      '*://api.exponea.com/bulk',
-      '*://api.infinario.com/bulk'
-    ]
+const filter = { // TODO: get from settings page and store in google.storage.sync
+  urls: [
+    '*://api.exponea.com/bulk',
+    '*://api.infinario.com/bulk'
+  ]
+};
+const opt_extraInfoSpec = ['requestBody'];
+
+chrome.webRequest.onBeforeRequest.addListener(onWebRequest, filter, opt_extraInfoSpec);
+
+function onWebRequest(details) {
+  console.log('onWebRequest', details);
+  try {
+    var tabId = details.tabId;
+    var buffer;
+    if (details && details.type === 'xmlhttprequest' && (buffer = details.requestBody.raw[0].bytes)) {
+      var body = arrayBufferToData.toJSON(buffer);
+      for (var i in body.commands) {
+        var cmd = body.commands[i];
+        processCommand(tabId, cmd);
+      }
+    }
+  } catch (e) {
+    console.error(e.stack);
+  }
+}
+
+function processCommand(tabId, cmd) {
+  const msg = {
+    type: 'command',
+    tabId: tabId,
+    cmd: cmd
   };
-  const opt_extraInfoSpec = ['requestBody'];
+  console.log(cmd.name, cmd.data.type, cmd.data);
+  backgroundPorts.forEach((port, index, array) => {
+    if (index === tabId) {
+      port.postMessage(msg);
+    }
+  });
+  addHistory(msg);
+}
 
-  chrome.webRequest.onBeforeRequest.addListener(onWebRequest, filter, opt_extraInfoSpec);
-
-  function onWebRequest(details) {
-    console.log('onWebRequest', details);
-    try {
-      var tabId = details.tabId;
-      var buffer;
-      if (details && details.type === 'xmlhttprequest' && (buffer = details.requestBody.raw[0].bytes)) {
-        var body = arrayBufferToData.toJSON(buffer);
-        for (var i in body.commands) {
-          var cmd = body.commands[i];
-          processCommand(tabId, cmd);
-        }
-      }
-    } catch (e) {
-      console.error(e.stack);
+function sendHistory(tabId) {
+  var port = backgroundPorts[tabId];
+  if (!port) {
+    return;
+  }
+  for (var i in messagesHistory) {
+    var msg = messagesHistory[i];
+    if (msg.tabId == tabId) {
+      port.postMessage(msg);
     }
   }
+}
 
-  function processCommand(tabId, cmd) {
-    const msg = {
-      type: 'command',
-      tabId: tabId,
-      cmd: cmd
-    };
-    console.log(cmd.name, cmd.data.type, cmd.data);
-    backgroundPorts.forEach((port, index, array) => {
-      if (index === tabId) {
-        port.postMessage(msg);
-      }
-    });
-    addHistory(msg);
+function addHistory(msg) {
+  if (messagesHistory.length >= MESSAGEHISTORY_LIMIT) {
+    console.count('addHistory[' + msg.tabId + '] discarding old message');
+    messagesHistory.shift();
   }
-
-  function sendHistory(tabId) {
-    var port = backgroundPorts[tabId];
-    if (!port) {
-      return;
-    }
-    for (var i in messagesHistory) {
-      var msg = messagesHistory[i];
-      if (msg.tabId == tabId) {
-        port.postMessage(msg);
-      }
-    }
-  }
-
-  function addHistory(msg) {
-    if (messagesHistory.length >= MESSAGEHISTORY_LIMIT) {
-      console.count('addHistory[' + msg.tabId + '] discarding old message');
-      messagesHistory.shift();
-    }
-    messagesHistory.push(msg);
-  }
-
-})();
+  messagesHistory.push(msg);
+}
